dd747507813f38d54253aea19a6a34ae62469e32
Eduardo Galeano <cegard689@gmail.com>
2020-11-27 23:37:21 -0500

    Base system finished

--- utils/helpers.go
+++ utils/helpers.go
@@ -8,11 +8,13 @@ import (
 
 func RandomInt(min int, max int) int {
     rand.Seed(time.Now().UnixNano())
+    
     return (rand.Intn(max - min) + min)
 }
 
 
 func RandomFloat() float64 {
     rand.Seed(time.Now().UnixNano())
+    
     return rand.Float64()
 }
