dab2d29e9dd7ae9dc901dc1179a21a5f8255eb8b
Eduardo Galeano <cegard689@gmail.com>
2020-11-29 15:54:23 -0500

    Main file simplified

--- components/ui.go
+++ components/ui.go
@@ -2,49 +2,38 @@ package components
 
 import (
     "fmt"
-    "bufio"
     "strconv"
     "strings"
 )
 
 
-func sendRequests(client *Client, loadBalancerInstance *loadBalancer, requestsNumber int) {
+func sendRequests(client *Client, requestsNumber int) {
     
     for index := 0; index < requestsNumber ; index++ {
         
-        loadBalancerInstance.AssignRequest(client.MakeRequest())
+        GetLoadBalancer().AssignRequest(client.MakeRequest())
     }
 }
 
 
-func ProcessUserCommands(client *Client, loadBalancerInstance *loadBalancer, in *bufio.Scanner) bool {
-    fmt.Println("Enter a command:")
-    fmt.Printf(">: ")
-    in.Scan()
-    input := strings.Split(in.Text(), " ")
+func ProcessUserCommands(client *Client, command string) {
+    input := strings.Split(command, " ")
     
     switch input[0] {
         
         case "send":
             requsetsNumbers, _ := strconv.Atoi(input[1])
-            sendRequests(client, loadBalancerInstance, requsetsNumbers)
-            return false
+            sendRequests(client, requsetsNumbers)
         
         case "serverStatus":
             fmt.Printf("\nProcessed requests so far: %d\n", len(client.ServerResponses))
-            loadBalancerInstance.PrintStatus()
-            return false
+            GetLoadBalancer().PrintStatus()
         
         case "serviceStatus":
             serviceId, _ := strconv.Atoi(input[1])
-            loadBalancerInstance.PrintServiceStatus(serviceId)
-            return false
-        
-        case "stop":
-            return true
+            GetLoadBalancer().PrintServiceStatus(serviceId)
         
         default:
             fmt.Printf("Command not recognized\n")
-            return false
     }
 }
