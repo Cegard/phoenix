269b2b7b847d915cda39b5c0d9ba40610be8351b
Eduardo Galeano <cegard689@gmail.com>
2020-12-09 22:06:51 -0500

    Main function prettier

--- phoenix/ui/ui.go
+++ phoenix/ui/ui.go
@@ -30,41 +30,53 @@ func joinStats (stats []string) string {
 }
 
 
+func processSendCommand (client *client.Client, requestsString string) (string, error) {
+    requestsNumber, err := strconv.Atoi(requestsString)
+    
+    if err == nil {
+        sendRequests(client, requestsNumber)
+        
+        return "", nil
+    } else {
+        
+        return utils.CouldNotProcessMsg, utils.NewNotNumberError(requestsString)
+    }
+}
+
+
+func processServiceStatusCommand (client *client.Client, idString string) (string, error) {
+    serviceId, err := strconv.Atoi(idString)
+    
+    if err == nil {
+        
+        return balancer.GetLoadBalancer().Stats.GetEntryInfo(serviceId), nil
+    } else {
+        
+        return utils.CouldNotProcessMsg, utils.NewNotNumberError(idString)
+    }
+}
+
+
 func ProcessUserCommands (client *client.Client, command string) (string, error) {
     var input = strings.Split(command, " ")
     
     switch input[0] {
         
         case fmt.Sprintf("%s", utils.Send):
-            requestsNumber, err := strconv.Atoi(input[1])
-            
-            if err == nil {
-                sendRequests(client, requestsNumber)
-                
-                return "", nil
-            } else {
-                
-                return utils.CouldNotProcessMsg, utils.NewNotNumberError(input[1])
-            }
+        
+            return processSendCommand(client, input[1])
         
         case fmt.Sprintf("%s", utils.ServerStatus):
             
             return fmt.Sprintf(
-                "\nProcessed requests so far: %d\n, %s",
+                "\nProcessed requests so far: %d %s",
                 len(client.GetResponses()),
                 joinStats(balancer.GetLoadBalancer().GetStatus()),
             ), nil
         
         case fmt.Sprintf("%s", utils.ServiceStatus):
-            serviceId, err := strconv.Atoi(input[1])
             
-            if err == nil {
-                
-                return balancer.GetLoadBalancer().Stats.GetEntryInfo(serviceId), nil
-            } else {
-                
-                return utils.CouldNotProcessMsg, utils.NewNotNumberError(input[1])
-            }
+            return processServiceStatusCommand(client, input[1])
         
         default:
             
